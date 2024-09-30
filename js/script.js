5a6d19f990b2166e6fb786a7ead56959ed95ec44
Harshsingh-31 <115529519+Harshsingh-31@users.noreply.github.com>
2024-09-30 15:27:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -147,29 +147,6 @@ function saveEdit() {
     const modal = bootstrap.Modal.getInstance(document.getElementById('editModal'));
     modal.hide();
 }
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('chem-supplies-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/script.js',
-          '/manifest.json',
-          '/icon-192x192.png',
-          '/icon-512x512.png',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
   
 
 window.onload = loadTableData;
